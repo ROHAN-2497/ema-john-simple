a62e667ed16b9d0ab0e017929ab9de9f6b2ac791
rohan future devoloper <xrohaislam373@gmail.com>
2023-06-15 01:15:06 +0600

    Order Cart

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Cart from "../Cart/Cart";
+import { useLoaderData } from "react-router-dom";
+
+const Orders = () => {
+    const cart = useLoaderData()
+    console.log(cart)
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h3> Orders Page : {cart.length}</h3>
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
